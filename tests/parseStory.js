b0e75843e4e75c13944cf16f7cb2f15a5c407d61
alban <alban@octopuce.fr>
2017-12-30 18:52:20 +0100

    [wip] starts being useful

--- tests/parseStory.js
+++ tests/parseStory.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const fs = require('fs');
 const path = require('path');
 const libinkle = require('../src/libinkle');
@@ -6,46 +7,68 @@ const assert = require('assert');
 const buf = fs.readFileSync(path.resolve( __dirname, 'stories/8v99.json'));
 describe('General functions', function() {
     it('should return an object', function(){
-        const story = libinkle.parse(buf.toString());
+        const inkle = new libinkle();
+        const story = inkle.parse(buf.toString());
         expect(story).to.be.an('object')
        }
       )
     it('should return stats given a string', function(){
-        const stats  = libinkle.stats(buf.toString());
+        const inkle = new libinkle();
+        const stats  = inkle.stats(buf.toString());
         expect(stats).to.be.an('object')
        }
       )
     it('should return stats given an object', function(){
-        const story = libinkle.parse(buf.toString());
-        const stats  = libinkle.stats(story);
+        const inkle = new libinkle();
+        const story = inkle.parse(buf.toString());
+        const stats  = inkle.stats(story);
         expect(stats).to.be.an('object')
         expect(stats.title).to.be.a('string')
         expect(stats.initial).to.be.a('string')
        }
-      )
-
+    )
 });
-describe('Agent', function() {
+describe('Story running', function() {
 
-    it('should return an agent', function(){
-        const agent = new libinkle.agent({name:"name"});
-        expect(agent).to.be.an('object')
-        expect(agent.name).to.be.a('string')
+    it('should return a story master', function(){
+        const inkle = new libinkle({name:"name"});
+        expect(inkle).to.be.an('object')
+        expect(inkle.name).to.be.a('string')
        }
    )
     it('should return a story from string', function(){
-        const agent = new libinkle.agent({source:buf.toString()});
-        expect(agent.story).to.be.an('object')
+        const inkle = new libinkle({source:buf.toString()});
+        expect(inkle.story).to.be.an('object')
        }
    )
     it('should return an initial stitch', function(){
-        const agent = new libinkle.agent({source:buf.toString()});
-        expect(agent.story.stats.initial).to.be.a('string')
-
+        const inkle = new libinkle({source:buf.toString()});
+        const initial = inkle.story.stats.initial;
+        expect(initial).to.be.a('string')
+        const stitch = inkle.story.getStitch(initial);
+        expect(stitch).to.be.an('object')
        }
    )
-
-
+    it('should start the story', function(){
+        const inkle = new libinkle({source:buf.toString()});
+        var success = inkle.start();
+        expect(success).to.be.equal(true,success)
+        var paragraphList = inkle.getText();
+        expect(paragraphList).to.be.an('array')
+        var choicesList = inkle.getChoices();
+        expect(choicesList).to.be.an('array')
+       }
+   )
+    it('should iterate through the story', function(){
+        const inkle = new libinkle({source:buf.toString()});
+        inkle.start();
+        const choicesList = inkle.getChoices();
+        const choice = _.keys(choicesList)[0]
+        expect(choice).to.be.an('string')
+        const success = inkle.choose(choice)
+        assert.deepEqual( _.keys(inkle.getChoices()), ['duHautDeSesMtres','vousRetournezSur'])
+       }
+   )
 });
 
 
